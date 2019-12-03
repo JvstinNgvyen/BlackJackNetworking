5a23537f2d9a04d5817f2aa216e72f32e6187710
nguyenj32 <44008241+nguyenj32@users.noreply.github.com>
2019-12-03 13:17:31 -0500

    Completed Source Code
    Added comments to client-side of Blackjack

--- clientBlackjackFinal.py
+++ clientBlackjackFinal.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
 import socket
-
-serverName = '192.168.0.4' #socket.gethostname()
+# Obtains Server IP address and Port Number
+serverName = '10.220.25.180' #socket.gethostname()
 serverPort = 5000
 sentence = input('Type "play" To Start Game ')
 
+# Loop that sends data to server
 while True:
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
     clientSocket.send(sentence.encode())
     game = clientSocket.recv(1024)
     print(game.decode())
-
+    # If the data from the server contains "Game Over" client closes
     if "Game Over" in game.decode():
         clientSocket.close()
         break
